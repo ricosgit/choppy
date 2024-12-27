22b70518fea0572f2b8f06997e971cf425d2997d
Riku <sasakiriku36@gmail.com>
2024-12-27 21:04:10 +0900

    Add: basic functions #1

--- choppy/app/page.tsx
+++ choppy/app/page.tsx
@@ -1,113 +1,117 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useRef, useState } from 'react';
 
-export default function Home() {
+const IndexPage = () => {
+  const audioContext= useRef<AudioContext | null>(null);
+  const analyser = useRef<AnalyserNode | null>(null);
+  const bufferLength = useRef<number | null>(null);
+  const dataArray = useRef<Uint8Array | null>(null);
+  const [isSpeaking, setIsSpeaking] = useState<boolean>(false);
+  const [showRandomImage, setShowRandomImage] = useState<boolean>(false);
+
+  const initAudioContext = async (): Promise<void> => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      audioContext.current = new AudioContext();
+      const source = audioContext.current.createMediaStreamSource(stream);
+      analyser.current = audioContext.current.createAnalyser();
+      source.connect(analyser.current);
+      analyser.current.connect(audioContext.current.destination);
+      analyser.current.fftSize = 256;
+      bufferLength.current = analyser.current.frequencyBinCount;
+      dataArray.current = new Uint8Array(bufferLength.current);
+    } catch (error) {
+      console.error('Error accessing microphone:', error);
+    }
+  };
+
+  const getMicrophoneVolume = (): number => {
+    if (!analyser.current || !dataArray.current || bufferLength.current === null) return 0;
+
+    analyser.current.getByteFrequencyData(dataArray.current);
+
+    // 正規化: dataArrayの最大値が255なので、それで割って0から1の範囲に変換
+    const normalizedVolume = dataArray.current.reduce(
+      (acc, value) => acc + value, 0
+    ) / (bufferLength.current * 255);
+
+    return normalizedVolume;
+  };
+
+  const handleMicrophoneVolumeChange = (): void => {
+    const volume = getMicrophoneVolume();
+    // console.log('Microphone Volume:', volume);
+
+    // 調整: 低い音でも反応しやすくするために閾値を調整
+    const speakingThreshold = 0.08;
+
+    if (volume > speakingThreshold) {
+      setIsSpeaking(true);
+    } else {
+      setIsSpeaking(false);
+    }
+  };
+
+  const showRandomImageAtRandomIntervals = (): void => {
+    const interval = Math.random() * (60000 / 20);
+    setTimeout(() => {
+      setShowRandomImage(true);
+      setTimeout(() => {
+        setShowRandomImage(false);
+      }, 1000);
+      showRandomImageAtRandomIntervals();
+    }, interval);
+  };
+
+  useEffect(() => {
+    initAudioContext();
+
+    return () => {
+      if (audioContext.current) {
+        audioContext.current.close().catch((error) => {
+          console.error('Error closing AudioContext:', error)
+        });
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const updateMicrophoneVolume = () => {
+      handleMicrophoneVolumeChange();
+    };
+
+    const updateInterval = setInterval(updateMicrophoneVolume, 100);
+
+    return () => {
+      clearInterval(updateInterval);
+
+      // AudioContextの終了
+      if (audioContext.current) {
+        audioContext.current.close().catch((error) =>{
+          console.error('Error closing AudioContext:', error);
+        })
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    showRandomImageAtRandomIntervals();
+
+    return () => {};
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h1>Microphone Volume Monitoring</h1>
+      {isSpeaking ? (
+        <img src="img/Talking_1000.png" alt="Speaking Image" style={{ background: 'green' }} />
+      ) : showRandomImage ? (
+        <img src="img/Blink_1000.png" alt="Random Image" style={{ background: 'green' }} />
+      ) : (
+        <img src="img/Normal_1000.png" alt="Default Image" style={{ background: 'green' }} />
+      )}
+    </div>
   );
-}
+};
+
+export default IndexPage;
